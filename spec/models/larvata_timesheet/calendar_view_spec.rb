b65695db3aadf5f992e7bee4929718b1769a5ddc
Frank Lam <ryzingsun11@yahoo.com>
2019-05-20 17:33:28 +0800

    Change CalendarView public api to call syntax

--- spec/models/larvata_timesheet/calendar_view_spec.rb
+++ spec/models/larvata_timesheet/calendar_view_spec.rb
@@ -2,18 +2,13 @@ require 'spec_helper'
 
 module LarvataTimesheet
   class CalendarView
-    attr_reader :date, :start_day
-
     START_DAY = :sunday
     DAYS = %i(sunday monday tuesday wednesday thursday friday saturday).freeze
 
-    def initialize(date, start_day = START_DAY)
+    def self.call(date, start_day = START_DAY)
       raise ArgumentError, "Invalid start day, please select one from #{DAYS}." unless DAYS.include?(start_day)
-      @date = Date.parse(date)
-      @start_day = start_day
-    end
 
-    def dates
+      date = Date.parse(date)
       start = date.beginning_of_month.beginning_of_week(start_day)
       finish = date.end_of_month.end_of_week(start_day)
       (start..finish).to_a
@@ -29,18 +24,18 @@ module LarvataTimesheet
       it 'has a collection of date like objects' do
         date = Time.zone.now
 
-        calendar = CalendarView.new(date.to_s(:f))
+        calendar = CalendarView.call(date.to_s(:f))
 
-        expect(day_count_range).to(include(calendar.dates.size))
+        expect(day_count_range).to(include(calendar.size))
       end
 
       context 'with the first day of a non-leap February falling on Sunday' do
         it 'spans 4 weeks (28 days)' do
           date = Time.zone.parse('2015-02-01')
 
-          calendar = CalendarView.new(date.to_s(:f))
+          calendar = CalendarView.call(date.to_s(:f))
 
-          expect(calendar.dates.size).to(eq(MIN_DAY_COUNT))
+          expect(calendar.size).to(eq(MIN_DAY_COUNT))
         end
       end
 
@@ -48,16 +43,16 @@ module LarvataTimesheet
         it 'spans 6 weeks (42 days)' do
           date = Time.zone.parse('2019-06-01')
 
-          calendar = CalendarView.new(date.to_s(:f))
+          calendar = CalendarView.call(date.to_s(:f))
 
-          expect(calendar.dates.size).to(eq(MAX_DAY_COUNT))
+          expect(calendar.size).to(eq(MAX_DAY_COUNT))
         end
       end
 
       it 'raises an error when given an invalid start day' do
         date = Time.zone.now
 
-        expect { CalendarView.new(date, 'ASDFGHJKL;') }.to(raise_error(ArgumentError))
+        expect { CalendarView.call(date, 'ASDFGHJKL;') }.to(raise_error(ArgumentError))
       end
     end
   end
