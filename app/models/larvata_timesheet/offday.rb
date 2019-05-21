dd473f16f775de5d3812adc8304044d010ceb2bb
Frank Lam <ryzingsun11@yahoo.com>
2019-05-21 11:04:28 +0800

    Add around_range scope to Offday

--- app/models/larvata_timesheet/offday.rb
+++ app/models/larvata_timesheet/offday.rb
@@ -4,5 +4,12 @@ module LarvataTimesheet
 
     validates :date_id, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: 'only allows YYYY-MM-DD format' }
     validates :is_off, inclusion: { in: [true, false], message: 'only allows a valid boolean value' }
+
+    def self.around_range(date_id)
+      date = Date.parse(date_id)
+      start = date.beginning_of_month.beginning_of_week
+      finish = date.end_of_month.end_of_week
+      where("date(date_id) BETWEEN ? AND ?", start, finish)
+    end
   end
 end
