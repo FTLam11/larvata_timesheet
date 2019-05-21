98f09133bc02562544cd63af4e62b28d6239739b
Frank Lam <ryzingsun11@yahoo.com>
2019-05-21 10:11:33 +0800

    Add inclusion validation on Offday#is_off

--- app/models/larvata_timesheet/offday.rb
+++ app/models/larvata_timesheet/offday.rb
@@ -3,5 +3,6 @@ module LarvataTimesheet
     self.primary_key = 'date_id'
 
     validates :date_id, format: { with: /\A\d{4}-\d{2}-\d{2}\z/, message: 'only allows YYYY-MM-DD format' }
+    validates :is_off, inclusion: { in: [true, false], message: 'only allows a valid boolean value' }
   end
 end
--- spec/models/larvata_timesheet/offday_spec.rb
+++ spec/models/larvata_timesheet/offday_spec.rb
@@ -11,5 +11,25 @@ module LarvataTimesheet
         expect(invalid_offday.valid?).to(be(false))
       end
     end
+
+    describe '#is_off' do
+      it 'has a valid boolean value do' do
+        expect { create(:offday, is_off: nil) }.to(raise_error(ActiveRecord::RecordInvalid))
+      end
+
+      context 'regardless of what day it is' do
+        it 'can be an off day' do
+          offday = build(:offday, is_off: true)
+
+          expect(offday.is_off).to(be(true))
+        end
+
+        it 'can be a workday' do
+          offday = build(:offday, is_off: false)
+
+          expect(offday.is_off).to(be(false))
+        end
+      end
+    end
   end
 end
