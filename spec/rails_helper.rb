b4d701a7acc711e02bb4d5461f96159e645ef393
Frank Lam <ryzingsun11@yahoo.com>
2019-05-21 18:01:17 +0800

    Add spec support request helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require File.expand_path('../dummy/config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 require 'support/factory_bot'
+require 'support/request_helper'
 require 'rails-controller-testing'
 require 'awesome_print'
 require 'pry'
---
+++ spec/support/request_helper.rb
@@ -0,0 +1,9 @@
+module RequestHelper
+  def body_content
+    JSON.parse(response.body)
+  end
+end
+
+RSpec.configure do |config|
+  config.include RequestHelper, type: :request
+end
