b46422517a4ee95adbfec4327618f92a706bdb66
Frank Lam <ryzingsun11@yahoo.com>
2019-05-23 14:30:53 +0800

    Add children association to Activity

--- app/models/larvata_timesheet/activity.rb
+++ app/models/larvata_timesheet/activity.rb
@@ -1,16 +1,22 @@
 module LarvataTimesheet
   class Activity < ApplicationRecord
-    belongs_to :category, class_name: 'LarvataTimesheet::Activity', optional: true
+    belongs_to :category, class_name: LarvataTimesheet::Activity, optional: true
+    has_many :children, -> { order(rank: :asc) }, class_name: LarvataTimesheet::Activity, foreign_key: 'category_id'
 
     validates_presence_of :rank, :name
     validates :rank, numericality: { greater_than_or_equal_to: 1 }
     validates :enabled, inclusion: { in: [true, false], message: 'only allows a valid boolean value' }
     validate :category_cannot_belong_to_self
+    validate :activity_cannot_have_grandfather
 
     private
 
     def category_cannot_belong_to_self
       errors.add(:category, "can't belong to itself") if category == self
+    end
+
+    def activity_cannot_have_grandfather
+      errors.add(:category, "does not support more than one generation") if category&.category
     end
   end
 end
--- spec/models/larvata_timesheet/activity_spec.rb
+++ spec/models/larvata_timesheet/activity_spec.rb
@@ -2,13 +2,30 @@ require 'rails_helper'
 
 module LarvataTimesheet
   RSpec.describe(Activity, type: :model) do
-    it 'may belong to a parent category' do
-      category = create(:activity, name: 'Leave')
+    describe 'associations do' do
+      it 'may belong to a parent category' do
+        category = create(:activity, name: 'Leave')
 
-      activity = create(:activity, category: category)
+        activity = create(:activity, category: category)
 
-      expect(category.category).to(be(nil))
-      expect(activity.category).to(be(category))
+        expect(category.category).to(be(nil))
+        expect(activity.category).to(be(category))
+      end
+
+      it 'may have children' do
+        category = create(:activity, name: 'Leave')
+        activity = create(:activity, category: category)
+
+        expect(category.children).to(include(activity))
+      end
+
+      it 'may only have one generation of children' do
+        category = create(:activity, name: 'Leave')
+        activity = create(:activity, category: category)
+        grandchild_activity = build(:activity, category: activity)
+
+        expect(grandchild_activity).to_not(be_valid)
+      end
     end
 
     it 'may not belong to itself' do
@@ -18,30 +35,32 @@ module LarvataTimesheet
       expect(category).to_not(be_valid)
     end
 
-    it 'has a positive rank' do
-      activity = build(:activity, rank: 1)
-      activity_with_negative_rank = build(:activity, rank: -1)
-      activity_with_nil_rank = build(:activity, rank: nil)
+    describe 'attributes' do
+      it 'has a positive rank' do
+        activity = build(:activity, rank: 1)
+        activity_with_negative_rank = build(:activity, rank: -1)
+        activity_with_nil_rank = build(:activity, rank: nil)
 
-      expect(activity.rank).to(eq(1))
-      expect(activity_with_negative_rank).to_not(be_valid)
-      expect(activity_with_nil_rank).to_not(be_valid)
-    end
+        expect(activity.rank).to(eq(1))
+        expect(activity_with_negative_rank).to_not(be_valid)
+        expect(activity_with_nil_rank).to_not(be_valid)
+      end
 
-    it 'has a name' do
-      activity = build(:activity, name: 'Earl')
-      activity_with_no_name = build(:activity, name: nil)
+      it 'has a name' do
+        activity = build(:activity, name: 'Earl')
+        activity_with_no_name = build(:activity, name: nil)
 
-      expect(activity.name).to(eq('Earl'))
-      expect(activity_with_no_name).to_not(be_valid)
-    end
+        expect(activity.name).to(eq('Earl'))
+        expect(activity_with_no_name).to_not(be_valid)
+      end
 
-    it 'may be enabled' do
-      activity = build(:activity, enabled: true)
-      bad_activity = build(:activity, enabled: nil)
+      it 'may be enabled' do
+        activity = build(:activity, enabled: true)
+        bad_activity = build(:activity, enabled: nil)
 
-      expect(activity.enabled).to(be(true))
-      expect(bad_activity).to_not(be_valid)
+        expect(activity.enabled).to(be(true))
+        expect(bad_activity).to_not(be_valid)
+      end
     end
   end
 end
