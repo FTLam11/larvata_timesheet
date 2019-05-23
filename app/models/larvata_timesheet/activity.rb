3071eaacdedc5b18766bda7ef88fdbe962431288
Frank Lam <ryzingsun11@yahoo.com>
2019-05-23 11:59:34 +0800

    Add rank validations for Activity

--- app/models/larvata_timesheet/activity.rb
+++ app/models/larvata_timesheet/activity.rb
@@ -2,6 +2,8 @@ module LarvataTimesheet
   class Activity < ApplicationRecord
     belongs_to :category, class_name: 'LarvataTimesheet::Activity', optional: true
 
+    validates_presence_of :rank
+    validates :rank, numericality: { greater_than_or_equal_to: 1 }
     validate :category_cannot_belong_to_self
 
     private
--- spec/models/larvata_timesheet/activity_spec.rb
+++ spec/models/larvata_timesheet/activity_spec.rb
@@ -16,5 +16,13 @@ module LarvataTimesheet
 
       expect { category.update!(category: category) }.to(raise_error(ActiveRecord::RecordInvalid))
     end
+
+    it 'has a positive rank' do
+      activity = build(:activity, rank: 1)
+
+      expect(activity.rank).to(eq(1))
+      expect { create(:activity, rank: -1) }.to(raise_error(ActiveRecord::RecordInvalid))
+      expect { create(:activity, rank: nil) }.to(raise_error(ActiveRecord::RecordInvalid))
+    end
   end
 end
