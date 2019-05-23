9e6d10dec1a873a46513cce70456aa0a881327a9
Frank Lam <ryzingsun11@yahoo.com>
2019-05-23 12:06:21 +0800

    Add name validations for Activity

--- app/models/larvata_timesheet/activity.rb
+++ app/models/larvata_timesheet/activity.rb
@@ -2,7 +2,7 @@ module LarvataTimesheet
   class Activity < ApplicationRecord
     belongs_to :category, class_name: 'LarvataTimesheet::Activity', optional: true
 
-    validates_presence_of :rank
+    validates_presence_of :rank, :name
     validates :rank, numericality: { greater_than_or_equal_to: 1 }
     validate :category_cannot_belong_to_self
 
--- spec/models/larvata_timesheet/activity_spec.rb
+++ spec/models/larvata_timesheet/activity_spec.rb
@@ -13,16 +13,27 @@ module LarvataTimesheet
 
     it 'may not belong to itself' do
       category = create(:activity)
+      category.category = category
 
-      expect { category.update!(category: category) }.to(raise_error(ActiveRecord::RecordInvalid))
+      expect(category).to_not(be_valid)
     end
 
     it 'has a positive rank' do
       activity = build(:activity, rank: 1)
+      activity_with_negative_rank = build(:activity, rank: -1)
+      activity_with_nil_rank = build(:activity, rank: nil)
 
       expect(activity.rank).to(eq(1))
-      expect { create(:activity, rank: -1) }.to(raise_error(ActiveRecord::RecordInvalid))
-      expect { create(:activity, rank: nil) }.to(raise_error(ActiveRecord::RecordInvalid))
+      expect(activity_with_negative_rank).to_not(be_valid)
+      expect(activity_with_nil_rank).to_not(be_valid)
+    end
+
+    it 'has a name' do
+      activity = build(:activity, name: 'Earl')
+      activity_with_no_name = build(:activity, name: nil)
+
+      expect(activity.name).to(eq('Earl'))
+      expect(activity_with_no_name).to_not(be_valid)
     end
   end
 end
