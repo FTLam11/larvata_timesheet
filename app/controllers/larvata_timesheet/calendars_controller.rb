2db538ac7df95c16e4bcfa7a6a36be4db45e176c
Frank Lam <ryzingsun11@yahoo.com>
2019-05-29 17:15:55 +0800

    Add destroy action for CalendarsController

--- app/controllers/larvata_timesheet/calendars_controller.rb
+++ app/controllers/larvata_timesheet/calendars_controller.rb
@@ -2,8 +2,24 @@ require_dependency "larvata_timesheet/application_controller"
 
 module LarvataTimesheet
   class CalendarsController < ApplicationController
+    before_action :set_calendar, only: [:destroy]
+
     def index
       render json: { data: Calendar.all }
+    end
+
+    def destroy
+      if @calendar.default
+        render json: { message: @calendar.errors.full_messages }, status: 400
+      else
+        @calendar.destroy
+      end
+    end
+
+    private
+
+    def set_calendar
+      @calendar = Calendar.find(params[:id])
     end
   end
 end
--- spec/controllers/larvata_timesheet/calendars_controller_spec.rb
+++ spec/controllers/larvata_timesheet/calendars_controller_spec.rb
@@ -12,5 +12,24 @@ module LarvataTimesheet
         expect(body_content["data"]).to(include(JSON.parse(calendar.to_json)))
       end
     end
+
+    describe 'DELETE calendars#destroy' do
+      it 'destroys non-default calendars' do
+        calendar = create(:calendar, default: false)
+
+        delete calendar_path(calendar)
+
+        expect { calendar.reload }.to(raise_error(ActiveRecord::RecordNotFound))
+        expect(response).to(have_http_status(204))
+      end
+
+      it 'does not destroy default calendars and responds with an error' do
+        calendar = create(:calendar, default: true)
+
+        delete calendar_path(calendar)
+
+        expect(response).to(have_http_status(400))
+      end
+    end
   end
 end
